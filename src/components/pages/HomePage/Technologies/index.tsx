4b5e77016f98e0eef1559cf501bfb663cf947a96
Lumenatom <lumenatom@gmail.com>
2023-04-05 15:36:57 +0300

    add technologies

--- src/components/pages/HomePage/Technologies/index.tsx
+++ src/components/pages/HomePage/Technologies/index.tsx
@@ -1,8 +1,49 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import styles from "./index.module.scss";
+import {
+  favoriteTechnologies,
+  otherTechnologies,
+  stylesTechnologies,
+  supportTechnologies,
+} from "./dataTechnologies";
 
 const Technologies: FC = () => {
-  return <div className={styles.technologiesWrapper}>HArd skill </div>;
+  return (
+    <div className={styles.technologiesWrapper}>
+      <div className={styles.favoriteTechnologies}>
+        {favoriteTechnologies.map((el) => (
+          <div>
+            <img src={el.img} alt={el.name} />
+            <h2>{el.name}</h2>
+          </div>
+        ))}
+      </div>
+      <div className={styles.stylesTechnologies}>
+        {stylesTechnologies.map((el) => (
+          <div>
+            <img src={el.img} alt={el.name} />
+            <h2>{el.name}</h2>
+          </div>
+        ))}
+      </div>
+      <div className={styles.supportTechnologies}>
+        {supportTechnologies.map((el) => (
+          <div>
+            <img src={el.img} alt={el.name} />
+            <h2>{el.name}</h2>
+          </div>
+        ))}
+      </div>
+      <div className={styles.otherTechnologies}>
+        {otherTechnologies.map((el) => (
+          <div>
+            <img src={el.img} alt={el.name} />
+            <h2>{el.name}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Technologies;
