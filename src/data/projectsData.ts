da347e003a323d939e2baaf991845dccca9ac2dd
Lumenatom <lumenatom@gmail.com>
2023-08-02 14:30:59 +0300

    add 3 project

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -11,9 +11,49 @@ import Pic_9 from "../assets/Projects/Project_2/pic_9.png";
 import Pic_10 from "../assets/Projects/Project_2/pic_10.png";
 import Pic_11 from "../assets/Projects/Project_2/pic_11.png";
 import Pic_12 from "../assets/Projects/Project_2/pic_12.png";
+
+import Pic_13 from "../assets/Projects/Project_3/pic_13.png";
+import Pic_14 from "../assets/Projects/Project_3/pic_14.png";
+import Pic_15 from "../assets/Projects/Project_3/pic_15.png";
+import Pic_16 from "../assets/Projects/Project_3/pic_16.png";
+import Pic_17 from "../assets/Projects/Project_3/pic_17.png";
+import Pic_18 from "../assets/Projects/Project_3/pic_18.png";
+import Pic_19 from "../assets/Projects/Project_3/pic_19.png";
+import Pic_20 from "../assets/Projects/Project_3/pic_20.png";
+import Pic_21 from "../assets/Projects/Project_3/pic_21.png";
 import { ProjectInfo } from "../types/types";
 
 export const projectsData: ProjectInfo[] = [
+  {
+    id: "3",
+    title: "Pomodoro Timer",
+    description:
+      "Pomodoro Timer is a handy tool for managing the hour. Set a short timer for focus, pause and long pause. Easily mingle between cycles, nail the focus and focus. Take awareness, if the skin cycle is completed, helping you to increase productivity and efficiency in work. In addition, give the opportunity to improve with those supplements, so that you can choose the best visual image.",
+    technology: ["React", "TypeScript", "hooks", "Styled components", "Vite"],
+    images: [
+      Pic_13,
+      Pic_14,
+      Pic_15,
+      Pic_16,
+      Pic_17,
+      Pic_18,
+      Pic_19,
+      Pic_20,
+      Pic_21,
+    ],
+    gitHub: "https://github.com/Lumenatom/Pomodoro_Timer",
+    link: "https://lumenatom.github.io/Pomodoro_Timer/",
+  },
+  {
+    id: "2",
+    title: "Minimalism To-Do List on React Native",
+    description:
+      "Minimalistic ToDo-list on React Native with Expo and Hooks - an effective add-on for task management on iOS and Android. Handy interface, adding, viewing, updating the list, hooks for productivity. Organize the work and do the special things manually and easily.",
+    technology: ["React Native", "TypeScript", "Framer Motion", "Hooks"],
+    images: [Pic_9, Pic_10, Pic_11, Pic_12],
+    gitHub: "https://github.com/Lumenatom/Minimalis-ToDo_React-Native",
+    link: "",
+  },
   {
     id: "1",
     title: "Clone web application McDonalds",
@@ -30,15 +70,5 @@ export const projectsData: ProjectInfo[] = [
     images: [Pic_1, Pic_2, Pic_3, Pic_4, Pic_5, Pic_6, Pic_7, Pic_8],
     gitHub: "https://github.com/Lumenatom/McDonalds_Application",
     link: "https://lumenatom.github.io/McDonalds_Application/",
-  },
-  {
-    id: "1",
-    title: "Minimalism To-Do List on React Native",
-    description:
-      "Minimalistic ToDo-list on React Native with Expo and Hooks - an effective add-on for task management on iOS and Android. Handy interface, adding, viewing, updating the list, hooks for productivity. Organize the work and do the special things manually and easily.",
-    technology: ["React Native", "TypeScript", "Framer Motion", "Hooks"],
-    images: [Pic_9, Pic_10, Pic_11, Pic_12],
-    gitHub: "https://github.com/Lumenatom/Minimalis-ToDo_React-Native",
-    link: "",
   },
 ];
